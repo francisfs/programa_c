90ec3bedba9eff2b9d3acc0fe8907958089c0b3d
francisfs <francis_2.5@hotmail.com>
2022-03-06 23:02:46 -0300

    logica_c

---
+++ etec/taboada.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int num = 7;
+       
+        for (int i = 0; i <= 10; i++)
+        {
+            printf("%d x %d = %d \n  ",num, i, i * num );
+        }
+        
+
+    return 0;
+}
